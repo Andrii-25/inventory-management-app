b2ff7e02bb9e3286f17f0ba19f0ddc1f890972d9
Andrii-25 <kotsupyr.a@gmail.com>
2021-11-14 12:30:15 +0200

    update index.js file in models and add item controller

---
+++ controllers/item.controller.js
@@ -0,0 +1,50 @@
+const { Item } = require("../models");
+
+module.exports = {
+  addItem: async function (req, res) {
+    try {
+      const { category, brand, model, price } = req.body;
+      const item = await Item.create({ category, brand, model, price });
+      res.status(201).json(item);
+    } catch (err) {
+      res.json({ message: err });
+    }
+  },
+
+  getAllItems: async function (req, res) {
+    try {
+      const items = await Item.findAll();
+      if (items.length === 0) {
+        res.json({ message: "There are no items!" });
+      } else {
+        res.status(200).json(items);
+      }
+    } catch (err) {
+      res.json({ message: err });
+    }
+  },
+
+  removeItem: async function (req, res) {
+    try {
+      const { id } = req.params;
+      const deletedItem = await Item.destroy({ where: { id } });
+      res.status(200).json(deletedItem);
+    } catch (err) {
+      res.json({ message: err });
+    }
+  },
+
+  updateItem: async function (req, res) {
+    try {
+      const { id } = req.query;
+      const { category, brand, model, price } = req.body;
+      const updatedItem = await Item.update(
+        { category, brand, model, price },
+        { where: { id } }
+      );
+      res.status(201).json(updatedItem);
+    } catch (err) {
+      res.json({ message: err });
+    }
+  },
+};
